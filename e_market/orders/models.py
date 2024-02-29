4b4bc9d53d988a28095719a8811bae8eaa9727f7
kiran raveendran <kiranraveendran50@gmail.com>
2024-02-29 12:24:58 +0530

    Initial data Model and template designing step

---
+++ e_market/orders/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from customers.models import Customer
+from products.models import product
+# Create your models here.
+
+
+class Order(models.Model):
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICES = ((LIVE, 'Live'), (DELETE, 'Delete'))
+    CART_STAGE = 0
+    ORDER_CONFIRMED = 1
+    ORDER_PROCESSED = 2
+    ORDER_DELIVERED = 3
+    ORDER_REJECTED = 4
+    STATUS_CHOICE = ((ORDER_PROCESSED, 'ORDER_PROCESSED'),
+                     (ORDER_DELIVERED, 'ORDER_DELIVERED'),
+                     (ORDER_REJECTED, 'ORDER_REJECTED'))
+    order_status = models.IntegerField(
+        choices=STATUS_CHOICE, default=CART_STAGE)
+    owner = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True, related_name='orders')
+    delete_status = models.IntegerField(choices=DELETE_CHOICES, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class OrderedItem(models.Model):
+    product = models.ForeignKey(
+        product, related_name='added_carts', on_delete=models.SET_NULL, null=True,)
+    quantity = models.IntegerField(default=1)
+    owner = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='added_items')
---
+++ e_market/themes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+
+class SiteStetting(models.Model):
+    banner = models.ImageField(upload_to='media/site/')
+    caption = models.TextField()
